965f0c7d9e528822ea1ace2ea962b877a40509d5
Nishant Singh <nishant@Nishants-MacBook-Pro.local>
2014-10-02 19:26:52 +0530

    adding PageMetaData model

--- spoonjoy/crawler/models.py
+++ spoonjoy/crawler/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class PageMetaData(models.Model):
+	title = models.CharField(max_length=200)
+	description = models.CharField(max_length=200)
+	keyword = models.CharField(max_length=200)
+
